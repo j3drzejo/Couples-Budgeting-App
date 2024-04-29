2ef23da9f0bab443d8e0d1e030f934a99d3a7cbf
j3drzejo <121938491+j3drzejo@users.noreply.github.com>
2024-04-29 10:00:02 +0200

    a

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import { Home } from './pages/Home'
+import { TopBar } from './pages/TopBar';
+
+function App() {
+  return (
+    <>
+      <TopBar/>
+      <Home/>
+
+    </>
+  );
+}
+
+export default App;
