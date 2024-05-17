0fb43e58b19550b3a8094f72204a8752af3c7d8a
j3drzejo <jirla30@gmail.com>
2024-05-17 12:26:24 +0200

    done smth

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,59 +1,18 @@
-import React, {useState} from 'react';
-import axios from 'axios';
+import React from 'react';
+import Home from './pages/Home';
+import Login from './pages/Login'
+import Register from './pages/Register';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const App = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-
-  const handleRegister = async () => {
-    try {
-      const response = await axios.post('http://localhost:3001/users/register', {
-        username: username,
-        password: password
-      });
-      
-      // If registration is successful, show alert
-      if (response.data.token) {
-        localStorage.setItem('token', response.data.token);
-        alert('Registration successful!');
-        setUsername('');
-        setPassword('');
-      }
-    } catch (error) {
-      // Handle error
-      console.error('Error during registration:', error);
-      alert('Registration failed. Please try again.');
-    }
-  };
-
-  
   return (
-    <div>
-      <h2>Register</h2>
-      <form>
-        <div>
-          <label>Username:</label>
-          <input
-            type="text"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
-        </div>
-        <div>
-          <label>Password:</label>
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-        <button type="button" onClick={handleRegister}>
-          Register
-        </button>
-      </form>
-
-      <h1>{localStorage.getItem('token')}</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,42 +1,81 @@
-import React, { useState } from 'react';
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Link from '@mui/material/Link';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
-function Login() {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  const handleLogin = () => {
-    // Simulate authentication (in a real application, this is where you would make an HTTP request to your backend)
-    if (username === 'admin' && password === 'password') {
-      setIsLoggedIn(true);
-    } else {
-      alert('Invalid username or password');
-    }
+const defaultTheme = createTheme();
+
+export default function Login() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get('email'),
+      password: data.get('password'),
+    });
   };
 
-
   return (
-    <div>
-      <div>
-        <h1>Login</h1>
-        <input
-          type="text"
-          placeholder="Username"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-        <br />
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <br />
-        <button onClick={handleLogin}>Login</button>
-      </div>
-    </div>
+    <ThemeProvider theme={defaultTheme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign In
+            </Button>
+            <Link href="#" variant="body2">
+                  {"Don't have an account? Sign Up"}
+            </Link>
+          </Box>
+        </Box>
+      </Container>
+    </ThemeProvider>
   );
 }
-
-export default Login;
