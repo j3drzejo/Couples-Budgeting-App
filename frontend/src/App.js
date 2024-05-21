a4a7ef011b8e580ff95fd7bdf9e63bb2ad306d33
j3drzejo <jirla30@gmail.com>
2024-05-21 17:25:34 +0200

    small changes xd

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ import Login from './pages/Login'
 import Register from './pages/Register';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-const App = () => {
+export default function App() {
   return (
     <BrowserRouter>
       <Routes>
@@ -15,5 +15,3 @@ const App = () => {
     </BrowserRouter>
   );
 }
-
-export default App;
