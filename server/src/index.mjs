7fa131888316bfdc6baadb36813f0299abc465b0
j3drzejo <jirla30@gmail.com>
2024-06-10 13:38:44 +0200

    cors

--- server/src/index.mjs
+++ server/src/index.mjs
@@ -1,11 +1,13 @@
 import express from 'express'
 import routes from './routes/index.mjs'
 import cookieParser from 'cookie-parser';
+import cors from 'cors'
 import 'dotenv/config'
 
 const app = express();
+app.use(cors());
 app.use(express.json());
-app.use(cookieParser())
+app.use(cookieParser());
 app.use(routes);
 
 const PORT = process.env.PORT || 3001;
